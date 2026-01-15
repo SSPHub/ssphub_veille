e7795eda62531eb4d9a60e80bdb7868ca2e61237
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-01-15 16:19:41 +0000

    with veille. in script

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
-from ssphub_veille.veille_function import *
+import veille_function as veille
 
 # To test
-extract_and_add_to_veille('ssphub_veille/export.json', extract_max_date(target_table='Test'), time_format_date=False, target_table='Test')
+# veille.extract_and_add_to_veille('export.json', veille.extract_max_date(target_table='Test'), time_format_date=False, target_table='Test')
 
 # To add automatically
-extract_and_add_to_veille('ssphub_veille/export.json', extract_max_date(), time_format_date=False, target_table='Veille')
+veille.extract_and_add_to_veille('export.json', veille.extract_max_date(), time_format_date=False, target_table='Veille')
 
 # To set a date
-# extract_and_add_to_veille('ssphub_veille/export.json', "2025-10-12", target_table='Veille')
+# veille.extract_and_add_to_veille('export.json', "2025-10-12", target_table='Veille')
