6a2dd829b12177d809e7aeb0bb504eb2a71947ff
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-02-17 09:08:18 +0000

    with function to add records without grist_api package

--- src/utils/access_grist_api.py
+++ src/utils/access_grist_api.py
@@ -1,6 +1,8 @@
-import polars as pl
-from grist_api import GristDocAPI  # To write into grist doc
 import os
+
+import polars as pl
+import requests
+from grist_api import GristDocAPI  # To write into grist doc
 
 
 def get_grist_api():
@@ -18,22 +20,48 @@ def get_grist_api():
     """
     # Log in to GRIST API
     SERVER = "https://grist.numerique.gouv.fr"
-    DOC_ID = os.environ['GRIST_VEILLE_DOC_ID']
+    DOC_ID = os.environ["GRIST_VEILLE_DOC_ID"]
 
-    if 'GRIST_API_KEY' not in os.environ:
+    if "GRIST_API_KEY" not in os.environ:
         raise ValueError("The GRIST_API_KEY environment variable does not exist.")
 
     # Returning API details connection
     return GristDocAPI(DOC_ID, server=SERVER)
 
 
-def download_table(table_id='Test'):
+def add_records(table_id, data_list):
+    if "GRIST_API_KEY" not in os.environ:
+        raise ValueError("The GRIST_API_KEY environment variable does not exist.")
+
+    SERVER = "https://grist.numerique.gouv.fr"
+    DOC_ID = os.environ["GRIST_VEILLE_DOC_ID"]
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.environ['GRIST_API_KEY']}",
+        "Content-Type": "application/json",
+    }
+
+    data_json = {
+        "records": data_list,
+    }
+
+    r = requests.post(
+        f"{SERVER}/api/docs/{DOC_ID}/Tables/{table_id}/records",
+        headers=headers,
+        json=data_json,
+    )
+
+    return r
+
+
+def download_table(table_id="Test"):
     """
     Fetch data from a Grist table
 
     Args:
         The grist table id
-        
+
 
     Returns:
         the table from Grist as a Polars DataFrame
