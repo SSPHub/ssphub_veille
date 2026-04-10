d7da1898c10434064172359703c908e1950e2c2b
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-04-10 14:03:36 +0000

    with command no Grist and only one room

--- src/tchap_bot/listeners/parser.py
+++ src/tchap_bot/listeners/parser.py
@@ -2,17 +2,20 @@ from src.tchap_bot.config import bot
 import simplematrixbotlib as botlib
 from src.make_data.clean_conv import clean_conv
 from src.grist.add_to_table import add_to_veille
+import os 
+
+no_grist="no Grist"
 
 @bot.listener.on_message_event
-async def example(room, message):
-    match = botlib.MessageMatch(room, message, bot)
+async def msg_to_Grist(room, message):
+    match = botlib.MessageMatch(os.environ["TCHAP_ROOM_ID"], message, bot)
 
-    if match.is_not_from_this_bot() and (match.contains('href') or match.contains('http')):
+    if match.is_not_from_this_bot() and (match.contains("href") or match.contains("http")) and not match.contains(no_grist):
 
         res_msg = add_to_veille(clean_conv(match.event))
 
         await bot.api.send_reaction(
-            room_id=room.room_id,
+            room_id=os.environ["TCHAP_ROOM_ID"],
             event=message,
             key="🔗"
         )
@@ -22,3 +25,9 @@ async def example(room, message):
             message=f"I added your message to Grist at row {res_msg}.",
             reply_to=match.event.event_id
         )
+
+    if match.is_not_from_this_bot() and match.contains(no_grist):
+        await bot.api.send_reaction(
+            room_id=os.environ["TCHAP_ROOM_ID"],
+            event=message,
+            key="❌")
