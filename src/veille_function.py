7b5249123f601cc4c9711c5ef6b8d5444db39fd6
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-04-10 09:40:57 +0000

    create add_to_table

--- src/veille_function.py
+++ src/veille_function.py
@@ -4,63 +4,10 @@ from src.make_data.clean_conv import clean_conv
 from src.data.formatting_time import convert_unix_time
 from src.utils.access_grist_api import GristApi
 from src.utils.logging import setup_logging
+from src.grist.add_to_table import add_to_veille
 
 # Regular expression to identify hyperlinks in Markdown format
 pattern = r"\[([^\]]+)\]\(([^)]+)\)"
-
-
-def add_to_veille(my_conv_df, target_table="Test", logger=setup_logging()):
-    """
-    add a dataframe to Veille grist table
-
-    Args:
-        a polars dataframe with records to update to Veille grist table.
-        Column names will be renamed to match target table column names.
-
-    Returns:
-        the records that have been added to table
-
-    Example:
-        >>> add_to_veille(............, target_table='Test')
-    """
-    # Rename
-    # Dictionnary for renaming variables / Right part must correspond to template keywords
-    variable_mapping = {
-        "link_text": "Titre_article",
-        "hyperlink": "Lien_article",
-        "sender": "Qui_a_propose",
-        "msg_link": "Quel_chanel",
-        "body": "Resume",
-        "origin_server_ts": "Date",
-    }
-
-    new_msg_df = my_conv_df.rename(variable_mapping).sort("Date")
-
-    # Export as dict to export to Grist
-    logger.info("Début de l'export de la table vers Grist")
-
-    new_msg_dict = (
-        new_msg_df.with_columns(pl.struct(new_msg_df.columns).alias("fields"))
-        .select("fields")
-        .to_dicts()
-    )
-
-    new_msg_json = {
-        "records": new_msg_dict,
-    }
-
-    res = GristApi().add_records(target_table, json=new_msg_json)
-    res = res.json().get("records", "")
-
-    if len(res) == 0:
-        res_msg = f"No record has been added to the {target_table} table"
-    else:
-        res_msg = f"{len(res)} records have been added to the {target_table} table, from id {res[0]['id']} to {res[-1]['id']}"
-
-    logger.info(f"{res_msg}")
-
-    logger.info("Fin de l'export de la table vers Grist")
-    return res_msg
 
 
 def extract_and_add_to_veille(
