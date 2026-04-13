c55de35d57497d4f912712d8049b10f192737e26
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-04-13 15:54:12 +0000

    update config

--- src/tchap_bot/config/settings.py
+++ src/tchap_bot/config/settings.py
@@ -1,40 +1,54 @@
 import os
 from dataclasses import dataclass
-from tchap_bot.core.room_filter import FilterMode
+from ..core.room_filter import FilterMode
 
 @dataclass(frozen=True)
 class Creds:
     homeserver: str
     username: str
     password: str
-    prefix: str
-    filter_mode: FilterMode
-    room_ids: list[str]
     session_stored_file: str
 
 
-class Config:
+@dataclass(frozen=True)
+class BotConfig:
     emoji_verify: bool
-    emoji_verify: bool
-    emoji_verify: bool
+    ignore_unverified_devices: bool
+    encryption_enabled: bool
+
+
+@dataclass(frozen=True)
+class Settings: 
+    prefix: str
+    filter_mode: FilterMode
+    room_ids: list[str]
+
+
+def load_all():
+    return load_creds(), load_config(), load_settings()
 
 
 def load_creds() -> Creds:
-    raw_ids = os.getenv("ROOM_IDS", "")
     return Creds(
         homeserver="https://matrix.agent.finances.tchap.gouv.fr",
         username=os.environ["TCHAP_BOT_SSPHUB_MATRIX_ID"],
         password=os.environ["TCHAP_BOT_SSPHUB_PWD"],
-        prefix=os.getenv("BOT_PREFIX", "!"),
-        filter_mode=FilterMode[os.getenv("ROOM_FILTER_MODE", "DISABLED")],
-        room_ids=[r.strip() for r in raw_ids.split(",") if r.strip()],
         session_stored_file="session.txt"
     )
 
 
-def load_config() -> Config:
-    return Config(
+def load_config() -> BotConfig:
+    return BotConfig(
         emoji_verify=True,
         ignore_unverified_devices=True,
         encryption_enabled=True
+    )
+
+
+def load_settings() -> Settings:
+    raw_ids = os.getenv("ROOM_IDS", "")
+    return Settings(
+        prefix=os.getenv("BOT_PREFIX", "!"),
+        filter_mode=FilterMode[os.getenv("ROOM_FILTER_MODE", "DISABLED")],
+        room_ids=[r.strip() for r in raw_ids.split(",") if r.strip()]
     )
