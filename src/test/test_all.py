18d624ad6bb648832063a9fa1166efa3aa198a2c
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-06-30 12:53:53 +0000

    COL_RESUME

--- src/test/test_all.py
+++ src/test/test_all.py
@@ -1,6 +1,6 @@
 from src.utils.access_grist_api import GristApi
 from src.extract import extract_and_add_to_veille
-from src.utils.config import _INTERNAL_PREFIXES, COL_LINK
+from src.utils.config import _INTERNAL_PREFIXES, COL_LINK, COL_TITLE, COL_RESUME
 
 def test_():
     return extract_and_add_to_veille("export.json", target_table="Test")
@@ -17,7 +17,7 @@ def test_add_records(**kwargs):
                 COL_TITLE: "TEST FROM ONYXIA - 2 Les Rencontres du Numérique Ouvert le 13/11",
                 COL_LINK: "https://docs.numerique.gouv.fr/dorerer/",
                 "Quel_chanel": _INTERNAL_PREFIXES[0] + "#/room/!DTuNyduZcTlsapzfyV:agent.finances.tchap.gouv.fr/$175974485023378hbhrc:agent.finances.tchap.gouv.fr",
-                "Resume": "",
+                COL_RESUME: "",
                 "Date": "2025-10-06 12:00",
             },
         },
@@ -26,7 +26,7 @@ def test_add_records(**kwargs):
                 COL_TITLE: "TEST FROM ONYXIA - vcvxcwcvwxxxw",
                 COL_LINK: "https://www.linkedin.com/posts/",
                 "Quel_chanel": _INTERNAL_PREFIXES[0] + "#/room/!DTuNyduZcTlsapzfyV:agent.finances.tchap.gouv.fr/$175978782747889EWpei:agent.finances.tchap.gouv.fr",
-                "Resume": "ht au cm d'e [qui a réussi](https://www.linkedin.com/) 🚀",
+                COL_RESUME: "ht au cm d'e [qui a réussi](https://www.linkedin.com/) 🚀",
                 "Date": "2025-10-06 23:57",
             },
         },
@@ -35,7 +35,7 @@ def test_add_records(**kwargs):
                 COL_TITLE: "TEST FROM ONYXIA - Webinaire eurostat: utilisation de l'ia pour la stat publique",
                 COL_LINK: "https://link.europa.eu",
                 "Quel_chanel": _INTERNAL_PREFIXES[0] + "#/room/!DTuNyduZcTlsapzfyV:agent.finances.tchap.gouv.fr/$1759935114134590jLsxw:agent.finances.tchap.gouv.fr",
-                "Resume": None,
+                COL_RESUME: None,
                 "Date": "2025-10-08 16:51",
             },
         },
